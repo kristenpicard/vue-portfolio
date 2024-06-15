8345096193b12d89a0e224d40030164e3476d231
Kristen Picard <kristenpicard93@gmail.com>
2024-06-15 13:15:57 -0500

    Removed navbar and made cursor cool

--- kristen-portfolio/src/App.vue
+++ kristen-portfolio/src/App.vue
@@ -1,35 +1,53 @@
 <template>
-  <SideNavbarVue />
+  <div class="custom-cursor"></div>
 </template>
 
 <script>
 import { ref } from 'vue';
-import SideNavbarVue from './components/SideNavbar.vue';
 
 export default {
   name: 'App',
   components: {
-    SideNavbarVue
+    
   },
   setup() {
     const message = ref('Welcome to Your Vue.js App');
-    
+    document.addEventListener('mousemove', (e) => {
+        const cursor = document.querySelector('.custom-cursor');
+        cursor.style.top = e.pageY + 'px';
+        cursor.style.left = e.pageX + 'px';
+    });
     return {
       message
     };
   }
 };
 </script>
-
 <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
+  width: 100vw;
+  height: 100vh;
+  background-color: #21022b;
 }
 
-body {
-  margin: unset;
-}
+  body {
+    margin: unset;
+    overflow: hidden;
+  }
+  .custom-cursor {
+    width: 1px;
+    height: 1px;
+    background-color: transparent;
+    border: 2px solid #333;
+    border-radius: 50%;
+    position: absolute;
+    pointer-events: none;
+    // z-index: 9999; /* Make sure the cursor is on top of everything */
+    box-shadow: 0px 0px 150px 30px rgb(250, 249, 249); /* Box shadow effect */
+  
+  }
 </style>
---
+++ kristen-portfolio/src/components/BodyContent.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="hello">
+    This will hold all the other components
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+  props: {
+    msg: String
+  },
+  setup(props) {
+    const message = ref(props.msg);
+    
+    return {
+      message
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
